332f611d2b6542ada8737b43f2569025b00182f3
msampaio2 <msampaio@stefanini.com>
2016-03-07 19:10:36 +0000

    With mongoose

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
+require('./config/database')('mongodb://localhost/contatooh');
+
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta ' +
 	app.get('port'));
